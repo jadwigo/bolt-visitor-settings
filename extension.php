bbdc5945660f281491f4e00b353b75f5e89dfa20
Lodewijk Evers <lodewijk@twokings.nl>
2013-01-08 16:09:27 +0100

    quick fix for dirty output

--- extension.php
+++ extension.php
@@ -40,11 +40,19 @@ function init($app)
     // autoloader does not pick it up automagically
     require_once( __DIR__."/src/Settings/Settings.php" );
 
+
     // Endpoint for VisitorSettings to get and put settings
-    $app->match("/visitorsettings/get", '\VisitorSettings\Controller::get')
-        ->bind('visitorsettingsget');
-    $app->match("/visitorsettings/put", '\VisitorSettings\Controller::put')
-        ->bind('visitorsettingsput');
+    $visitorsettings_controller = $app['controllers_factory'];
+    $visitorsettings_controller
+        ->match('/get', '\VisitorSettings\Controller::get')
+        ->bind('visitorsettingsget')
+        ;
+    $visitorsettings_controller
+        ->match('/put', '\VisitorSettings\Controller::put')
+        ->bind('visitorsettingsput')
+        ;
+    $app->mount('/async/visitorsettings', $visitorsettings_controller);
+
 }
 
 class Controller
@@ -67,10 +75,9 @@ class Controller
             $settings = $visitorsettings->load( $visitor_id, $key );
 
             return $app->json($settings, 200);
-            //return new Response(json_encode($settings), 200, array('Cache-Control' => 's-maxage=3600, public'));
         } else {
+
             return $app->json(false, 404);
-            //return new Response(json_encode(false), 404, array('Cache-Control' => 's-maxage=3600, public'));
         }
     }
     
@@ -97,7 +104,6 @@ class Controller
         }
 
         return $app->json('OK', 201);
-        //return new Response(json_encode('OK'), 200, array('Cache-Control' => 's-maxage=3600, public'));
     }
 
 }
